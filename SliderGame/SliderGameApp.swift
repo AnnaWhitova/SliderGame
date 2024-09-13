0a7eecf1e3d91d901410fc3eab85d864634103ed
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-09-13 19:06:29 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Анна Белова on 13.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
